dfb1cc6f93bb59054ddb0ef8d99538798bf4932b
Ko Shine <kanekitheghoul4@gmail.com>
2022-01-30 01:57:58 +0630

    night

---
+++ app/Location.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    //
+    protected $fillable = ['name'];
+    public function wayplan() {
+        return $this->belongsTo('App\WayPlanSchedule');
+    }
+}
---
+++ app/WayPlanSchedule.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WayPlanSchedule extends Model
+{
+    //
+    protected $fillable = [
+        'customer_name',
+        'customer_phone',
+        'receive_point',
+        'receive_address',
+        'receive_date',
+        'dropoff_point',
+        'dropoff_address',
+        'dropoff_date',
+        'tracking_id',
+        'remark',
+        'parcel_quantity',
+        'total_weight',
+        'per_kg_charges',
+        'package_id',
+        'total_charges',
+        'receive_status',
+        'dropoff_status',
+        'rider_id',
+        'customer_address',
+        'customer_status',
+        'myawady_status',
+        'myawady_date',
+        'customer_date',
+        'token',
+        'way_status',
+        'receive_remark',
+        'dropoff_remark',
+        'myawady_remark',
+        'customer_remark',
+        'reject_status',
+        'reject_date',
+        'register_date',
+            ];
+            public function receivelocation() {
+                return $this->belongsTo('App\Location','receive_point');
+            }
+            public function dropofflocation() {
+                return $this->belongsTo('App\Location','dropoff_point');
+            }
+}
